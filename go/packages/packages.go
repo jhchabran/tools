732b53e02c504f93ec4d632a500bf5d2753ec7ae
Jean-Hadrien Chabran <jh@chabran.fr>
2023-02-16 10:19:40 +0100

    fixup

--- go/packages/packages.go
+++ go/packages/packages.go
@@ -257,6 +257,7 @@ type driverResponse struct {
 // proceeding with further analysis. The PrintErrors function is
 // provided for convenient display of all errors.
 func Load(cfg *Config, patterns ...string) ([]*Package, error) {
+	panic("here")
 	l := newLoader(cfg)
 	response, err := defaultDriver(&l.Config, patterns...)
 	if err != nil {
